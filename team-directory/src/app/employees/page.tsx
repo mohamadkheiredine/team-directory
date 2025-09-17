bbf862e6a4d6ecc24a49e927cf7b7e8ca96a5bcf
mohamadkheiredine <kheiredinemhmd@gmail.com>
2025-09-17 14:55:41 +0300

    fix link employees

--- team-directory/src/app/employees/page.tsx
+++ team-directory/src/app/employees/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import EmployeesTable from "../components/features/employee-table";
 
 export default async function EmployeesPage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/employees`);
+  const res = await fetch(`http://localhost:3000/api/employees`);
   const employees = await res.json();
 
   return (
